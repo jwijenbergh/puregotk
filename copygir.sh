fc75be7eb418ebf55ec5e64809854e85f07bbd71
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-10-17 21:24:37 -0700

    Switch `copygir.sh` script from using Alpine Linux downstream to extracting GIR files to extracting them from the upstream runtime directly
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- copygir.sh
+++ copygir.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-podman build . -t puregotk-gir-fetcher -f gir.Dockerfile
-podman create --name puregotk-gir-fetcher puregotk-gir-fetcher
-
-GIR_DIR="internal/gir/spec/"
-find "${GIR_DIR}"*.gir -type f | while read filename; do
-  podman cp "puregotk-gir-fetcher:/gir-files/$(basename ${filename})" "${GIR_DIR}" || true
-done
-podman rm puregotk-gir-fetcher
+for f in internal/gir/spec/*.gir; do flatpak run --filesystem="${PWD}" --command=sh org.gnome.Sdk -c "cp /usr/share/gir-1.0/$(basename ${f}) ${PWD}/${f}"; done
