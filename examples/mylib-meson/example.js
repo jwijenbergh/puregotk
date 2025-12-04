7ce65e556f09aef2b8a1b852fabca2bc36eb483c
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-12-03 23:47:11 -0800

    Examples: Add mylib-meson example
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

---
+++ examples/mylib-meson/example.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S gjs -m
+
+import Adw from "gi://Adw?version=1";
+import GObject from "gi://GObject";
+import Gio from "gi://Gio";
+import MyLibMeson from "gi://MyLibMeson?version=0.1";
+import system from "system";
+
+const ExampleApplication = GObject.registerClass(
+  {
+    GTypeName: "ExampleApplication",
+  },
+  class ExampleApplication extends Adw.Application {
+    constructor() {
+      super({
+        application_id: "io.github.puregotk.MyLibMesonExample",
+        flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
+      });
+    }
+
+    #window = null;
+
+    vfunc_activate() {
+      this.#window = new MyLibMeson.MainApplicationWindow({
+        application: this,
+      });
+
+      this.#window.connect("button-test-clicked", () => {
+        console.log("Test button clicked");
+
+        this.#window.show_toast("Button was clicked!");
+        this.#window.test_button_sensitive = false;
+
+        setTimeout(() => {
+          this.#window.show_toast("Button re-enabled after 3 seconds");
+          this.#window.test_button_sensitive = true;
+        }, 3000);
+      });
+
+      this.#window.present();
+    }
+  }
+);
+
+new ExampleApplication().run([system.programInvocationName, ...ARGV]);
---
+++ examples/mylib-meson/src/mylib-meson.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#define MYLIB_MESON_INSIDE
+
+#include "mylib-meson-version.h"
+#include "window.h"
+
+#undef MYLIB_MESON_INSIDE
+
+G_END_DECLS
